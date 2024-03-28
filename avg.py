b904d158b74f6a9628fb59bb3a744856f5f167e1
Rolly Maulana  Awangga <rolly@awang.ga>
2024-03-28 09:18:54 +0700

    avg

--- avg.py
+++ avg.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Replace '/path/to/your/VAE.csv' with the actual path to your CSV file.
 # For example: df = pd.read_csv('/mnt/data/VAE.csv')
-df = pd.read_csv('/path/to/your/VAE.csv')
+df = pd.read_csv('V1_1x1_mse\VAE.csv')
 
 # Calculate the average (mean) of the column values. This assumes your CSV has a single column of numerical values.
 average_value = df.mean()[0]
